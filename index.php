412b4d7119ae361a7068818bd0e6cd75164acf67
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-08-27 17:19:00 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-bitcoin:/index.php
+ *
+ * @creation  2019-08-27
+ * @version   1.0
+ * @package   unit-bitcoin
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/Bitcoin.class.php');
