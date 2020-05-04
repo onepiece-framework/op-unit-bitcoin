8ed9ff25f81b4d4df46e8ac363ada7c0df5f661d
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2020-05-05 02:26:16 +0900

    New: testcase > index.php

---
+++ testcase/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * op-unit-bitcoin:/Bitcoin.class.php
+ *
+ * @created   2020-05-05
+ * @package   op-unit-bitcoin
+ * @version   1.0
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\UNIT;
+
+/** use
+ *
+ */
+use OP\Config;
+use function OP\Unit;
+
+//  ...
+$config = Config::Get('bitcoin');
+
+/* @var $bitcoin Bitcoin */
+$bitcoin = Unit('Bitcoin');
+
+//	...
+$temp['balance'] = $bitcoin->Balance();
+
+//  ...
+D($config, $temp);
