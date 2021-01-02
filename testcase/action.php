94b72d05eccbdb78c18a5f11d5a70396180a6d27
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2021-01-02 19:04:08 +0900

    New: testcase > action.php

---
+++ testcase/action.php
@@ -0,0 +1,25 @@
+<?php
+/** op-unit-bitcoin:/testcase/action.php
+ *
+ * @created   2020-12-25
+ * @package   op-unit-bitcoin
+ * @version   1.0
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\BITCOIN;
+
+/** use
+ *
+ */
+use function OP\Unit;
+use function OP\Load;
+use function OP\Template;
+
+//	...
+Load('Template');
+Template('index.php');
