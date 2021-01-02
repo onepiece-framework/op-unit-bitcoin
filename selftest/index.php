ee82fd77867fc4165096194456aad797bcfa3a92
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2021-01-02 18:38:25 +0900

    New: selftest > index.php

---
+++ selftest/index.php
@@ -0,0 +1,9 @@
+<?php
+/** op-unit-bitcoin:/selftest/index.php
+ *
+ * @created   2021-01-02
+ * @version   1.0
+ * @package   op-unit-bitcoin
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
